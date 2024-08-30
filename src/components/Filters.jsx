01181d9a48ec4d488cb51e8c2b1aafdd3ea60abd
Mauricio Covarrubias <mauricioecovarrubias@hotmail.com>
2024-08-29 21:30:59 -0600

    Added changes for shopping car

---
+++ src/components/Filters.jsx
@@ -0,0 +1,47 @@
+import { useState, useId } from 'react'
+import './Filters.css'
+import { useFilters } from '../hooks/useFilters'
+
+export function Filters () {
+  const {setFilters, filters} = useFilters()
+  const minPriceFilterId = useId()
+  const categoryFilterId = useId()
+
+  const handleChangeMinPrice = (event) => {
+    setFilters(prevState => ({
+      ...prevState,
+      minPrice: event.target.value
+    }))
+  }
+
+  const handleChangeCategory  = (event) => {
+    setFilters(prevState => ({
+      ...prevState,
+      category: event.target.value
+    }))
+  }
+  return (
+    <section className="filters">
+      <div>
+        <label htmlFor="minPriceFilterId">Precio a partir de: </label>
+        <input 
+          type="range"
+          id={minPriceFilterId}
+          min='0'
+          max='1000'
+          onChange={handleChangeMinPrice}
+          value={filters.minPrice}
+        />
+        <span>${filters.minPrice}</span>
+      </div>
+      <div>
+        <label htmlFor="categoryFilterId">Categoría</label>
+        <select id={categoryFilterId} onChange={handleChangeCategory}>
+          <option value="all">Todas</option>
+          <option value="fragrances">Fragancias</option>
+          <option value="furniture">Muebles</option>
+        </select>
+      </div>
+    </section>
+  )
+}
